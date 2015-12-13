21fa358396fa4e29335585eaab0a80a17442aea6
Nick Cooper <envy3d@gmail.com>
2015-12-13 06:14:24 -0800

    Friction and Pulse

---
+++ Assets/Scripts/Pulse.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pulse : MonoBehaviour
+{
+    public AnimationCurve pulseForce;
+
+    private bool pulseActive = false;
+
+    public void OnTriggerStay(Collider other)
+    {
+        if (pulseActive)
+        {
+            Rigidbody rb = other.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                float force = pulseForce.Evaluate((other.transform.position - transform.position).magnitude);
+                rb.AddForce((other.transform.position - transform.position).normalized * force, ForceMode.Impulse);
+            }
+        }
+    }
+
+    public void ActivatePulse()
+    {
+        pulseActive = true;
+        StartCoroutine(DeactivatePulse());
+    }
+
+    private IEnumerator DeactivatePulse()
+    {
+        //Debug.Log("Pulse Start");
+        yield return new WaitForFixedUpdate();
+        yield return new WaitForFixedUpdate();
+        pulseActive = false;
+        //Debug.Log("Pulse End");
+    }
+}
